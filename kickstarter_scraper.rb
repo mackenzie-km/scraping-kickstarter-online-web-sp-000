9476b6cf8e4c958a771c65b3699b720176f185dd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-08 23:58:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,12 +9,13 @@ projects = kickstarter.css("li.project.grid_4")
 
 projects.each do |project|
 title = project.css("h2.bbcard_name strong a").text
+projects[title.to_sym] = {}
 project.css("p.bbcard_blurb").text
 project.css("span.location-name").text
 project.css("li.first.funded").text
 
 #binding.pry
-projects[titles.to_sym] = {}
+
 
 end 
 
