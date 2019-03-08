7c0fcfbf42b5b7ac8ac013e7e07f9f9bcb98df82
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-08 23:41:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ projects = kickstarter.css("li.project.grid_4")
 further_in = projects.css("h2.bbcard_name strong a").text
 description = projects.css("p.bbcard_blurb").text
 locations = projects.css("span.location-name").text
+percent_funded = 
 binding.pry
 further_in
 end
