8419bd155540b532093fc1c22fefa50c85b39fbe
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-08 23:15:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,9 @@ require "pry"
 def create_project_hash
 html = File.read("fixtures/kickstarter.html")
 kickstarter =  Nokogiri::HTML(html)
-projects = kickstarter.css("li.project-grid-4").first.text
+projects = kickstarter.css("li.project-grid-4")
   binding.pry 
 end
+
+#projects:
+kickstarter.css("li.project-grid-4")
