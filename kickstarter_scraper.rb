b65a2e93de9cffb6bbac8e21c538ec2b108b32b2
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-09 00:07:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ kickstarter.css("li.project.grid_4").collect do |project|
 title = project.css("h2.bbcard_name strong a").text
 
 project_hash[title.to_sym] = {
-:image_link => project.css("p.bbcard_blurb").text,
+:image_link => project.css("div.project-thumbnail a img").attribute("src").value,
 :description => project.css("h2.bbcard_name strong a").text,
 :location => project.css("span.location-name").text,
 :percent_funded => project.css("li.first.funded").text.gsub("%","").to_i
