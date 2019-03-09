72cac0c9d81d2fb33d22f821c6b2b9688fd72af6
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-09 00:03:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,14 +2,14 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  projects = {}
+  project_hash = {}
 html = File.read("fixtures/kickstarter.html")
 kickstarter =  Nokogiri::HTML(html)
 projects = kickstarter.css("li.project.grid_4")
 
 projects.each do |project|
 title = project.css("h2.bbcard_name strong a").text
-projects[title.to_sym] = {
+project_hash[title.to_sym] = {
 :image_link => project.css("p.bbcard_blurb"),
 :description => project.css("h2.bbcard_name strong a"),
 :location => project.css("span.location-name").text,
